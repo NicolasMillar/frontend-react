f82a5db7630c9f00628331a4c3145d7589b17859
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-25 15:14:55 -0400

    estilos login

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,7 +1,19 @@
 export default function Login() {
+
+    const onSubmit = (ev) => {
+        ev.prventDefault()
+    }
+
     return (
-        <div>
-            Login
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Iniciar sesion en tu cuenta</h1>
+                    <input type="text" placeholder="Rut" />
+                    <input type="password" placeholder="Contraseña" />
+                    <button className="btn btn-block">LogIn</button>    
+                </form>
+            </div>
         </div>
     );
 }
