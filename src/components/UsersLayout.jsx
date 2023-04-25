a3cc646fd7d71ade7a2c79e3dc59310191c787df
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-25 14:47:46 -0400

    proteccion de rutas

--- src/components/UsersLayout.jsx
+++ src/components/UsersLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
 
 export default function UsersLayout() {
+    const {user, token} = useStateContext();
+
+    if(!token){
+        return <Navigate to="/login" />
+    }
+
     return (
         <div>
             <Outlet />
