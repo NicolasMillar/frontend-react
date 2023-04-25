635a13be04d2ca755d78db42f759876321b685c8
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-24 22:39:30 -0400

    algunas rutas

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/login";
+import Users from "./views/User";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 
 const router = createBrowserRouter([
-    
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/users',
+        element: <Users />
+    },
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '*',
+        element: <NotFound />   
+    },
 ])
 
 export default router;
