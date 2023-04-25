8f10bb82fd765921445fa5491428a12ea80395b6
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-24 23:04:36 -0400

    creacion de los archivos layout

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,21 +2,43 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/login";
 import Users from "./views/User";
 import Home from "./views/Home";
+import HomeLayout from "./components/HomeLayout";
 import NotFound from "./views/NotFound";
+import LoginLayout from "./components/LoginLayout";
+import UsersLayout from "./components/UsersLayout";
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <HomeLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+        ]
     },
     {
         path: '/',
-        element: <Home />
+        element: <LoginLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+        ]
     },
+    {
+        path: '/',
+        element: <UsersLayout />,
+        children: [
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
+    },
+    
     {
         path: '*',
         element: <NotFound />   
