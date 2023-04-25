22e5f304383cfa9c8863af67e0127707200e8620
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-25 15:05:35 -0400

    un poco de estilo

--- src/components/UsersLayout.jsx
+++ src/components/UsersLayout.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, Link } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
 
 export default function UsersLayout() {
@@ -8,9 +8,29 @@ export default function UsersLayout() {
         return <Navigate to="/login" />
     }
 
+    const onLogout = (ev) => {
+        ev.prventDefault()
+    }
+
     return (
-        <div>
-            <Outlet />
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/">Algo voy a poner aca</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        {user.name}
+                        <a className="btn-logout" href="#" onClick={onLogout}>Logout</a>
+                    </div>
+                </header>
+                 <main>
+                    <Outlet /> 
+                </main>
+            </div>
         </div>
     );
 }
