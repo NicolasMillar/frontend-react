7967d93d74d2955df3be1f72c28d13e600d444d8
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-05-03 15:04:10 -0400

    login deveria funcionar falta correccion de cors

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,7 +1,32 @@
+import { useRef,useState } from "react";
+import { useStateContext } from "../context/ContextProvider";
+import axiosClient from "../axiosClient";
+
+
 export default function Login() {
+    const rutRef = useRef();
+    const passwordRef = useRef();
+    const { setUser, setToken } = useStateContext()
+    const [message, setMessage] = useState(null)
 
     const onSubmit = (ev) => {
-        ev.prventDefault()
+        ev.preventDefault();
+        const payload = {
+            rut: rutRef.current.value,
+            password: passwordRef.current.value,
+            userType: "administrador",
+        }
+        console.log(payload);
+        axiosClient.post('/login', payload)
+        .then(({data}) => {
+            setUser(data.user)
+            setToken(data.token);
+        }).catch((err) => {
+            const response = err.response;
+            if (response && response.status === 422) {
+                setMessage(response.data.message)
+            }
+        }) 
     }
 
     return (
@@ -9,8 +34,13 @@ export default function Login() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Iniciar sesion en tu cuenta</h1>
-                    <input type="text" placeholder="Rut" />
-                    <input type="password" placeholder="Contraseña" />
+                    {message &&
+                        <div className="alert">
+                            <p>{message}</p>
+                        </div>
+                    }
+                    <input ref={rutRef} type="text" placeholder="Rut" />
+                    <input ref={passwordRef} type="password" placeholder="Contraseña" />
                     <button className="btn btn-block">LogIn</button>    
                 </form>
             </div>
